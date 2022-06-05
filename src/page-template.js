c93e49a67cbc48a6a25f78ac63c5bdf4a32791e3
Mehran Matin <81208724+MehranMatin@users.noreply.github.com>
2022-06-05 14:19:01 -0400

    code snapshot

--- src/page-template.js
+++ src/page-template.js
@@ -12,6 +12,7 @@ const generateAbout = aboutText => {
     `;
 };
 
+// create the projects section
 const generateProjects = projectsArr => {
     return`
         <section class="my-3" id="portfolio">
@@ -53,57 +54,10 @@ const generateProjects = projectsArr => {
             </div>
         </section>
     `;
-    
-    // get array of just featured projects
-    const featuredProjects = projectsArr.filter(project => {
-        if (project.feature) {
-            return true;
-        } else {
-            return false;
-        }
-    });
-
-    // get array of all non-featured projects
-    const nonFeaturedProjects = projectsArr.filter(project => {
-        if (!project.feature) {
-            return true;
-        } else {
-            return false;
-        }
-    });
-
-    const featuredProjectHtmlArr = featuredProjects.map(({ name, description, languages, link }) => {
-        return`
-        <div class="col-12 mb-2 bg-dark text-light p-3 flex-column">
-            <h3 class="portfolio-item-title text-light">${name}</h3>
-            <h5 class="portfolio-languages">
-                Built with:
-                ${languages.join(', ')}
-            </h5>
-            <p>${description}</p>
-            <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
-        </div>
-        `;
-    });
-
-    const nonFeaturedProjectHtmlArr = nonFeaturedProjects.map(({ name, description, languages, link }) => {
-        return`
-        <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
-            <h3 class="portfolio-item-title text-light">${name}</h3>
-            <h5 class="portfolio-languages">
-                Built with:
-                ${languages.join(', ')}
-            </h5>
-            <p>${description}</p>
-            <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
-        </div>
-        `;
-    });
 };
 
+// export functions to generate entire page
 module.exports = templateData => {
-    console.log(templateData);
-
     // destructure projects and about data from templateData based on their property key names
     const { projects, about, ...header } = templateData;``
     
